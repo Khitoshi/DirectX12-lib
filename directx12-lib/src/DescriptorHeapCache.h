7c877944f16223bf009767897b9dcb8730842b0c
Khitoshi <kubotah1115@gmail.com>
2023-07-27 14:30:34 +0900

    ディスクリプタヒープキャッシュのリファクタリング完了

--- directx12-lib/src/DescriptorHeapCache.h
+++ directx12-lib/src/DescriptorHeapCache.h
@@ -5,41 +5,57 @@
 class DescriptorHeapCache
 {
 public:
+    /// <summary>
+    /// キャッシュ生成時の設定
+    /// </summary>
     struct DescriptorHeapCacheConf
     {
-        ID3D12Resource* resource;
-        UINT slot;
+        ID3D12Resource* resource;   //リソース
+        UINT slot;                  //スロット
     };
-
 
 public:
     DescriptorHeapCache() {}
     ~DescriptorHeapCache() {}
 
+    /// <summary>
+    /// 取得or作成
+    /// </summary>
+    /// <param name="device">GPUデバイス</param>
+    /// <param name="conf">生成時の設定</param>
+    /// <param name="descriptorHeap">ディスクリプタヒープ</param>
+    /// <param name="desc">SRVの設定</param>
     void getOrCreate(ID3D12Device* device, DescriptorHeapCacheConf conf, ID3D12DescriptorHeap* descriptorHeap, D3D12_SHADER_RESOURCE_VIEW_DESC desc)
     {
-
-        auto it = handleMapCache.find(conf.slot);
-        if (it != handleMapCache.end() && it->second == conf.resource) {
+        //すでに作成済みの場合は何もしない
+        auto it = this->handleMapCache_.find(conf.slot);
+        if (it != this->handleMapCache_.end() && it->second == conf.resource) {
             return;
         }
 
+        //作成
         create(device, conf, descriptorHeap, desc);
     }
 
 private:
 
+    /// <summary>
+    /// リソース作成
+    /// </summary>
+    /// <param name="device">GPUデバイス</param>
+    /// <param name="conf">生成時の設定</param>
+    /// <param name="descriptorHeap">ディスクリプタヒープ</param>
+    /// <param name="desc">SRVの設定</param>
     void create(ID3D12Device* device, DescriptorHeapCacheConf conf, ID3D12DescriptorHeap* descriptorHeap, D3D12_SHADER_RESOURCE_VIEW_DESC desc)
     {
         D3D12_CPU_DESCRIPTOR_HANDLE handle = descriptorHeap->GetCPUDescriptorHandleForHeapStart();
         handle.ptr += device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV) * conf.slot;
 
         device->CreateShaderResourceView(conf.resource, &desc, handle);
-        handleMapCache[conf.slot] = conf.resource;
+        handleMapCache_[conf.slot] = conf.resource;
     }
 
 private:
-
-    std::map<UINT, ID3D12Resource*> handleMapCache;
+    std::map<UINT, ID3D12Resource*> handleMapCache_;    //slotをキーにしたリソースのマップ
 
 };
